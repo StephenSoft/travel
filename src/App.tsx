40f1d2d3e1e83705fec43cba25bcf9ee4ec9cda6
StephenZzz <stephenzhao94@163.com>
2022-11-08 01:00:13 +0800

    add index menu and carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import styles from "./App.module.css";
-import { Header } from './components';
-import { Footer } from './components';
+import { Header, Footer, SideMenu, Carousel } from './components';
+import { Row, Col } from 'antd'
 
 function App() {
   return (
     <div className={styles.App}>
       <Header/>
-      <Footer/>
+      <div className={styles["page-content"]}>
+          <Row style={{marginTop: 20}}>
+            <Col span={6}>
+              <SideMenu />
+            </Col>
+            <Col span={18}>
+              <Carousel />
+            </Col>
+          </Row>
+      </div>
+      <Footer/> 
     </div>
   );
 }
